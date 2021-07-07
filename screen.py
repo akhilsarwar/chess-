dd7bab035305f5e73aa0e7be484d04b8a4c353cc
akhilsarwar <64355950+akhilsarwar@users.noreply.github.com>
2021-07-07 23:29:33 +0530

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,12 +1,8 @@
 import pygame as pg
 
 class Screen:
-    screen_height = 400
-    screen_width = 400
+    screen_height = 600
+    screen_width = 600
     cell_dim = screen_height // 8
     fract = (8, 10)
     piece_dim = (cell_dim * fract[0]) // fract[1]
-
-screen_ = pg.display.set_mode((Screen.screen_width, Screen.screen_height))
-
-
